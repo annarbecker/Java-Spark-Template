a0d37a993eb260f11dc859a5ac09b340fdf132a7
Anna Becker and Daren Schaad <student@epicodus.com>
2016-02-16 08:19:03 -0800

    Update java files with filter constructor.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,6 +3,7 @@ import java.util.HashMap;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
+import static org.fluentlenium.core.filter.FilterConstructor.*;
 
 
 public class App {
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.fluentlenium.core.filter.FilterConstructor.*;
+
 
 public class AppTest extends FluentTest {
   public WebDriver webDriver = new HtmlUnitDriver();
