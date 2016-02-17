301e8cf4483c3b797de9096def1d77812f0e9ab3
Anna Becker <anna.r.becker@gmail.com>
2016-02-16 19:12:07 -0800

    Update App.java imports.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,7 +3,6 @@ import java.util.HashMap;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
-import static org.fluentlenium.core.filter.FilterConstructor.*;
 
 
 public class App {
